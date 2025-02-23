15af1deaafa454e9f31bbb77958214a1dfdf80d4
Johana Salas <juanitafsp@hotmail.com>
2025-02-23 06:58:42 -0500

    actualiza 1 app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log ("Ejecutando el sistema")
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let titulo = document.querySelector ("h1");
+titulo.inertHTML = "Amigo secreto";
+let parrafo = document.querySelector ("p");
+parrafo.innerHTML = "Digite el nombre de sus amigos"
+function agregarAmigo (){
+alert ("click desde el botón");
+}
+function sorteoAmigo (){
+    alert ("¡Has hecho click en el botón!");
+}
+
